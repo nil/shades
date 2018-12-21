eac9b35105d5d4e3b24e9ce07b7512a4c8a83885
Nil Vila <nilvilamar@gmail.com>
2018-12-21 16:04:55 +0100

    :wrench: Added aliases for Jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
     'vue'
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^c/(.*)$': '<rootDir>/components/$1',
+    '^a/(.*)$': '<rootDir>/assets/$1',
+    '^js/(.*)$': '<rootDir>/src/js/$1',
+    '^store(.*)$': '<rootDir>/src/store.js'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
