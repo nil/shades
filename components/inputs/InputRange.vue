f30d7fde9f1af95f9256345877e4e6c3a60f95fa
Nil Vila <nilvilamar@gmail.com>
2019-01-27 13:47:58 +0100

    :children_crossing: Range inputs can now be modified with a number field

--- components/inputs/InputRange.vue
+++ components/inputs/InputRange.vue
@@ -2,7 +2,14 @@
   <div class="input-range">
     <div class="input-range--info">
       <div class="input-range--name">{{ label }}</div>
-      <div class="input-range--value">{{ value }}</div>
+      <input type="text" class="input-range--value"
+        ref="input"
+        :value="value"
+        tabindex="-1"
+        @blur="writeUpdate($event)"
+        @keydown.enter="writeUpdate($event)"
+        @keydown.up="arrowUpdate($event)"
+        @keydown.down="arrowUpdate($event)" />
     </div>
     <div class="input-range--slider">
       <div class="input-range--path">
@@ -20,6 +27,7 @@
 
 <script>
 import store from 'store';
+import numFormatter from 'js/numFormatter';
 
 export default {
   name: 'InputRange',
@@ -29,6 +37,30 @@ export default {
     id: String,
     min: [String, Number],
     max: [String, Number]
+  },
+
+  methods: {
+    writeUpdate(e, value) {
+      const chosenValue = e ? e.target.value : value;
+      const val = numFormatter(chosenValue, this.max, this.min);
+
+      if (val || val === 0) {
+        store.commit('updateRange', {
+          id: this.id,
+          value: val
+        });
+        this.$refs.input.value = val;
+      } else {
+        this.$refs.input.value = this.value;
+      }
+    },
+
+    arrowUpdate(e) {
+      const diff = 1 + e.shiftKey * 9;
+      const val = this.value + (e.key === 'ArrowDown' ? -diff : diff);
+
+      this.writeUpdate(undefined, val);
+    }
   },
 
   computed: {
