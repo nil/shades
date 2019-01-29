f85acbbcbce3d3214b20e50ba4bf66bb0d125d17
Nil Vila <nilvilamar@gmail.com>
2019-01-29 14:22:43 +0100

    :bulb: Added docs on isSectionActive

--- src/js/isSectionActive.js
+++ src/js/isSectionActive.js
@@ -1,5 +1,12 @@
 import store from 'store';
 
+/**
+ * Checks if section with name `id` is active
+ *
+ * @param {string} id - The name of the section
+ *
+ * @returns {boolean} Returns `true` if section is active or undefined, else `false`.
+ */
 export default function (id) {
   const active = store.state[id].active;
 
